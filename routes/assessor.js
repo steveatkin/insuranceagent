d1b2b53d75e7cb731b103c6a1093008650ecac44
Steven Atkin <atkin@us.ibm.com>
2016-10-10 18:59:02 -0400

    Lock down assessor page

--- routes/assessor.js
+++ routes/assessor.js
@@ -1,8 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+function ensureAuthenticated(req, res, next) {
+  if (!req.isAuthenticated()) {
+    req.session.originalUrl = req.originalUrl;
+    res.redirect('/login');
+  } else {
+    return next();
+  }
+}
+
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', ensureAuthenticated, function(req, res, next) {
   res.render('assessor');
 });
 
