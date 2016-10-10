8d0991f34e780c008659b1775fccf1a4a91bcb27
Steven Atkin <atkin@us.ibm.com>
2016-10-10 15:35:14 -0400

    Added authentication

--- app.js
+++ app.js
@@ -31,10 +31,11 @@ app.set('view engine', 'jade');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(__dirname + '/public'));
-
 
 app.use('/', routes);
 app.use('/assessor', assessor);
@@ -49,7 +50,7 @@ app.use('/dataservices', data);
 app.use('/twilio', twilio);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
@@ -60,7 +61,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+  app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
@@ -71,13 +72,12 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
     error: {}
   });
 });
-
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,113 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
+var optional = require('optional');
+var appEnv = require('cfenv').getAppEnv();
+var cfEnvUtil = require('./cfenv-credsbylabel');
+var OpenIDConnectStrategy = require('passport-idaas-openidconnect').IDaaSOIDCStrategy;
+
+var expressSession = require('express-session');
+var sessionStore = new expressSession.MemoryStore;
+
+router.use(expressSession({
+  secret: 'somesecretmagicword',
+  resave: 'true',
+  saveUninitialized: 'true',
+  store: sessionStore
+}))
+
+router.use(passport.initialize());
+router.use(passport.session());
+
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function (obj, done) {
+  done(null, obj);
+});
+
+var serviceRegex = /(SingleSignOn).*/;
+
+var options = optional('./sso-credentials.json') || {
+  appEnv: appEnv
+};
+
+// parse vcap using cfenv if available
+if (options.appEnv && !options.credentials) {
+  options.credentials = cfEnvUtil.getServiceCredsByLabel(options.appEnv, serviceRegex);
+}
+// try again with name
+else if (options.appEnv && !options.credentials) {
+  options.credentials = options.appEnv.getServiceCreds(serviceRegex);
+}
+
+var client_id = options.credentials.clientId;
+var client_secret = options.credentials.secret;
+var authorization_url = options.credentials.authorizationEndpointUrl;
+var token_url = options.credentials.tokenEndpointUrl;
+var issuer_id = options.credentials.issuerIdentifier;
+
+// you MUST change the host route to match your application name
+var callback_url = 'https://insuranceagent.mybluemix.net/auth/sso/callback';
+
+var OpenIDConnectStrategy = require('passport-idaas-openidconnect').IDaaSOIDCStrategy;
+var Strategy = new OpenIDConnectStrategy({
+    authorizationURL: authorization_url,
+    tokenURL: token_url,
+    clientID: client_id,
+    scope: 'openid',
+    response_type: 'code',
+    clientSecret: client_secret,
+    callbackURL: callback_url,
+    skipUserProfile: true,
+    issuer: issuer_id
+  },
+  function (accessToken, refreshToken, profile, done) {
+    process.nextTick(function () {
+      profile.accessToken = accessToken;
+      profile.refreshToken = refreshToken;
+      done(null, profile);
+    })
+  });
+
+passport.use(Strategy);
+
+
+router.get('/login', passport.authenticate('openidconnect', {}));
+
+function ensureAuthenticated(req, res, next) {
+  if (!req.isAuthenticated()) {
+    req.session.originalUrl = req.originalUrl;
+    res.redirect('/login');
+  } else {
+    return next();
+  }
+}
+
+router.get('/auth/sso/callback', function (req, res, next) {
+  var redirect_url = req.session.originalUrl;
+  passport.authenticate('openidconnect', {
+    successRedirect: redirect_url,
+    failureRedirect: '/failure',
+  })(req, res, next);
+});
+
+
+router.get('/hello', ensureAuthenticated, function (req, res) {
+  res.send('Hello, ' + req.user['id'] + '!');
+});
+
+router.get('/failure', function (req, res) {
+  res.send('login failed');
+});
+
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index');
 });
+
 
 module.exports = router;
