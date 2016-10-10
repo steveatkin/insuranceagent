c621820ca9e4be54e91139ebd611964fd919b9bc
Steven Atkin <atkin@us.ibm.com>
2016-10-10 19:44:34 -0400

    Added redis for session

--- routes/index.js
+++ routes/index.js
@@ -7,13 +7,63 @@ var cfEnvUtil = require('./cfenv-credsbylabel');
 var OpenIDConnectStrategy = require('passport-idaas-openidconnect').IDaaSOIDCStrategy;
 
 var expressSession = require('express-session');
-var sessionStore = new expressSession.MemoryStore;
+//var sessionStore = new expressSession.MemoryStore;
+
+var redis = require('redis');
+var RedisStore = require('connect-redis')(expressSession);
+
+
+var serviceSSORegex = /(SingleSignOn).*/;
+var serviceRedisRegex = /(compose-for-redis).*/;
+
+var optionsSSO = optional('./sso-credentials.json') || {
+  appEnv: appEnv
+};
+
+var optionsRedis = optional('./redis-credentials.json') || {
+  appEnv: appEnv
+};
+
+// parse vcap using cfenv if available
+if (optionsSSO.appEnv && !optionsSSO.credentials) {
+  optionsSSO.credentials = cfEnvUtil.getServiceCredsByLabel(options.appEnv, serviceSSORegex);
+}
+// try again with name
+else if (optionsSSO.appEnv && !optionsSSO.credentials) {
+  optionsSSO.credentials = optionsSSO.appEnv.getServiceCreds(serviceSSORegex);
+}
+
+// parse vcap using cfenv if available
+if (optionsRedis.appEnv && !optionsRedis.credentials) {
+  optionsRedis.credentials = cfEnvUtil.getServiceCredsByLabel(options.appEnv, serviceRedisRegex);
+}
+// try again with name
+else if (optionsRedis.appEnv && !optionsRedis.credentials) {
+  optionsRedus.credentials = optionsRedis.appEnv.getServiceCreds(serviceRedisRegex);
+}
+
+var client_id = optionsSSO.credentials.clientId;
+var client_secret = optionsSSO.credentials.secret;
+var authorization_url = optionsSSO.credentials.authorizationEndpointUrl;
+var token_url = optionsSSO.credentials.tokenEndpointUrl;
+var issuer_id = optionsSSO.credentials.issuerIdentifier;
+var redis_url = optionsRedis.credentials.uri;
+
+var redisClient = redis.createClient(redis_url);
+
+redisClient.on('error', function (err) {
+    console.log('Error ' + err);
+});
+
+redisClient.on('connect', function() {
+    console.log('Connected to Redis');
+});
 
 router.use(expressSession({
   secret: 'somesecretmagicword',
   resave: 'true',
   saveUninitialized: 'true',
-  store: sessionStore
+  store: new RedisStore({ client: redisClient })
 }))
 
 router.use(passport.initialize());
@@ -27,26 +77,9 @@ passport.deserializeUser(function (obj, done) {
   done(null, obj);
 });
 
-var serviceRegex = /(SingleSignOn).*/;
 
-var options = optional('./sso-credentials.json') || {
-  appEnv: appEnv
-};
 
-// parse vcap using cfenv if available
-if (options.appEnv && !options.credentials) {
-  options.credentials = cfEnvUtil.getServiceCredsByLabel(options.appEnv, serviceRegex);
-}
-// try again with name
-else if (options.appEnv && !options.credentials) {
-  options.credentials = options.appEnv.getServiceCreds(serviceRegex);
-}
 
-var client_id = options.credentials.clientId;
-var client_secret = options.credentials.secret;
-var authorization_url = options.credentials.authorizationEndpointUrl;
-var token_url = options.credentials.tokenEndpointUrl;
-var issuer_id = options.credentials.issuerIdentifier;
 
 // you MUST change the host route to match your application name
 var callback_url = 'https://insuranceagent.mybluemix.net/auth/sso/callback';
