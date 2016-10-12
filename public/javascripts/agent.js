9ef035eda0032086e69d091ea2f2cdf71000f94d
Steven Atkin <atkin@us.ibm.com>
2016-10-12 08:15:38 -0400

    Added history data table check

--- public/javascripts/agent.js
+++ public/javascripts/agent.js
@@ -23,22 +23,27 @@
 function initMap() {
   // Try HTML5 geolocation.
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
+    navigator.geolocation.getCurrentPosition(function (position) {
       var pos = {
         lat: position.coords.latitude,
         lng: position.coords.longitude
       };
 
       var map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: pos.lat, lng: pos.lng},
+        center: {
+          lat: pos.lat,
+          lng: pos.lng
+        },
         zoom: 6
       });
-      var infoWindow = new google.maps.InfoWindow({map: map});
+      var infoWindow = new google.maps.InfoWindow({
+        map: map
+      });
 
       //infoWindow.setPosition(pos);
       infoWindow.setContent('Location found.');
       map.setCenter(pos);
-    }, function() {
+    }, function () {
       handleLocationError(true, infoWindow, map.getCenter());
     });
   } else {
@@ -50,31 +55,31 @@ function initMap() {
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
   infoWindow.setPosition(pos);
   infoWindow.setContent(browserHasGeolocation ?
-                        'Error: The Geolocation service failed.' :
-                        'Error: Your browser doesn\'t support geolocation.');
+    'Error: The Geolocation service failed.' :
+    'Error: Your browser doesn\'t support geolocation.');
 }
 
 function setupTable() {
-    $.extend($.fn.bootstrapTable.defaults, {
-        formatNoMatches: function () {
-            return Resources.getResources().noHistory;
-        }
-    });
+  $.extend($.fn.bootstrapTable.defaults, {
+    formatNoMatches: function () {
+      return Resources.getResources().noHistory;
+    }
+  });
 
-    $("#history-table").bootstrapTable({
-        columns: [{
-            field: "role",
-            title: Resources.getResources().role
-        }, {
-            field: "owner",
-            title: Resources.getResources().owner
-        }, {
-            field: "date",
-            title: Resources.getResources().date
-        }]
-    });
+  $("#history-table").bootstrapTable({
+    columns: [{
+      field: "role",
+      title: Resources.getResources().role
+    }, {
+      field: "owner",
+      title: Resources.getResources().owner
+    }, {
+      field: "date",
+      title: Resources.getResources().date
+    }]
+  });
 
-    $("#history-table").bootstrapTable('load', []);
+  $("#history-table").bootstrapTable('load', []);
 }
 
 function getHistory(policy) {
@@ -83,50 +88,50 @@ function getHistory(policy) {
   var spinner = new Spinner().spin(target);
 
   $.ajax({
-      type: "GET",
-      url: "/dataservices/claim-history/" + policy,
-      success: function(data) {
-          // reset the history table
-          $("#history-table").bootstrapTable('load', []);
+    type: "GET",
+    url: "/dataservices/claim-history/" + policy,
+    success: function (data) {
+      // reset the history table
+      $("#history-table").bootstrapTable('load', []);
 
-          if(data.claim.state) {
-            $("#current-state").val(data.claim.state);
-          }
-          else {
-            $("#current-state").val(Resources.getResources().received);
-          }
-
-          data.claim.history.forEach(function (value) {
-              $("#history-table").bootstrapTable('append', [{
-                    role: value.role,
-                    owner: value.owner,
-                    date: (new Date(value.date)).toUTCString()
-              }]);
-          });
-
-          spinner.stop();
-      },
-      error: function(xhr, message) {
-          alert(message);
+      if (data.claim.state) {
+        $("#current-state").val(data.claim.state);
+      } else {
+        $("#current-state").val(Resources.getResources().received);
       }
-    });  
+
+      if (data.claim.history) {
+        data.claim.history.forEach(function (value) {
+          $("#history-table").bootstrapTable('append', [{
+            role: value.role,
+            owner: value.owner,
+            date: (new Date(value.date)).toUTCString()
+          }]);
+        });
+      }
+
+      spinner.stop();
+    },
+    error: function (xhr, message) {
+      alert(message);
+    }
+  });
 }
 
 $(document).ready(function () {
   var policyInformation = null;
 
   // Register the enter key to the go button
-  $("input").bind("keydown", function(event) {
-      var keycode = (event.keyCode ? event.keyCode : (event.which ? event.which : event.charCode));
-      if (keycode == 13) { // keycode for enter key
-         // force the 'Enter Key' to implicitly click the Update button
-         $('#insurance-query-button').click();
-         return false;
-      } 
-      else  {
-         return true;
-      }
-   });
+  $("input").bind("keydown", function (event) {
+    var keycode = (event.keyCode ? event.keyCode : (event.which ? event.which : event.charCode));
+    if (keycode == 13) { // keycode for enter key
+      // force the 'Enter Key' to implicitly click the Update button
+      $('#insurance-query-button').click();
+      return false;
+    } else {
+      return true;
+    }
+  });
 
   // Listen for accordion events
   $('#accordion').on('show.bs.collapse', function (e) {
@@ -137,10 +142,10 @@ $(document).ready(function () {
 
 
   var userLang = (navigator.language ||
-                  navigator.userLanguage).substring(0,2).toLowerCase();
+    navigator.userLanguage).substring(0, 2).toLowerCase();
 
   // listen for the click of the ask question button
-  $('#insurance-query-button').click(function() {
+  $('#insurance-query-button').click(function () {
     var context = {};
     var latestResponse = Conversation.getResponsePayload();
     if (latestResponse) {
@@ -168,10 +173,10 @@ $(document).ready(function () {
     type: "GET",
     url: "/resources",
     data: {
-      "resource":  "agent",
-      "language":  userLang
+      "resource": "agent",
+      "language": userLang
     },
-    success: function(data) {
+    success: function (data) {
       Resources.setResources(data);
 
       $("#title").text(data.title);
@@ -203,10 +208,10 @@ $(document).ready(function () {
 
       // Create the history table after we know our resources are loaded
       setupTable();
-      
+
     },
-    error: function(xhr, message) {
-        alert(message);
+    error: function (xhr, message) {
+      alert(message);
     }
   });
 
@@ -214,7 +219,7 @@ $(document).ready(function () {
   // Setup the callback that is invoked when a policy is found.
   var policyResponsePayloadSetter = Policy.setResponsePayload;
 
-  Policy.setResponsePayload = function(data) {
+  Policy.setResponsePayload = function (data) {
     policyResponsePayloadSetter.call(Policy, data);
 
     policyInformation = data;
@@ -222,7 +227,7 @@ $(document).ready(function () {
     // Clear the weather alerts boxes
     $("#forecast").val('');
     $("#recommendation").val('');
-    
+
     $("#policy-type").val(data.policyType);
     $("#months-since-claim").val(data.monthsSinceLastClaim);
     $("#number-of-policies").val(data.numberOfPolicies);
@@ -232,13 +237,17 @@ $(document).ready(function () {
     $("#reason").val(data.claimReason);
 
     // Localize the display of the currencies
-    if(typeof Intl != "undefined") {
+    if (typeof Intl != "undefined") {
       var claim = new Number(data.totalClaimAmount);
       var premium = new Number(data.monthlyPremiumAuto);
-      data.totalClaimAmount = new Intl.NumberFormat('default',
-            { style: 'currency', currency: 'USD' }).format(claim);
-      data.monthlyPremiumAuto = new Intl.NumberFormat('default',
-            { style: 'currency', currency: 'USD' }).format(premium);     
+      data.totalClaimAmount = new Intl.NumberFormat('default', {
+        style: 'currency',
+        currency: 'USD'
+      }).format(claim);
+      data.monthlyPremiumAuto = new Intl.NumberFormat('default', {
+        style: 'currency',
+        currency: 'USD'
+      }).format(premium);
     }
 
     $("#monthly-premium").val(data.monthlyPremiumAuto);
@@ -252,7 +261,7 @@ $(document).ready(function () {
 
     // Translate the offers text
 
-    if(userLang != 'en') {
+    if (userLang != 'en') {
       $.ajax({
         type: "GET",
         url: "/translate",
@@ -260,26 +269,25 @@ $(document).ready(function () {
           "text": data.message,
           "language": userLang
         },
-        success: function(data) {
+        success: function (data) {
           $("#special-offers").val(data);
         },
-        error: function(xhr, message) {
+        error: function (xhr, message) {
           $("#special-offers").val(data.message);
         }
       });
-    }
-    else {
+    } else {
       $("#special-offers").val(data.message);
     }
 
     // setup the callback to respond to the weather alerts
     var weatherResponsePayloadSetter = Weather.setResponsePayload;
 
-    Weather.setResponsePayload = function(newPayloadStr) {
+    Weather.setResponsePayload = function (newPayloadStr) {
       weatherResponsePayloadSetter.call(Weather, newPayloadStr);
-      if(newPayloadStr) {
+      if (newPayloadStr) {
         $("#forecast").val($("#forecast").val() + newPayloadStr.description);
-        if(newPayloadStr.instruction) {
+        if (newPayloadStr.instruction) {
           $("#recommendations").val($("#recommendations").val() + newPayloadStr.instruction);
         }
       }
@@ -293,10 +301,16 @@ $(document).ready(function () {
     Weather.getWeather(data.latitude, data.longitude, userLang);
 
     // Show the policy holder's location on the map
-    var geo = {lat: parseFloat(data.latitude), lng: parseFloat(data.longitude)};
+    var geo = {
+      lat: parseFloat(data.latitude),
+      lng: parseFloat(data.longitude)
+    };
     var mapDiv = document.getElementById('map');
     var map = new google.maps.Map(mapDiv, {
-      center: {lat: geo.lat, lng: geo.lng},
+      center: {
+        lat: geo.lat,
+        lng: geo.lng
+      },
       zoom: 8
     });
 
@@ -305,13 +319,13 @@ $(document).ready(function () {
   // Setup the conversation callback that gets invoked when a response is received.
   var conversationResponsePayloadSetter = Conversation.setResponsePayload;
 
-  Conversation.setResponsePayload = function(newPayloadStr) {
+  Conversation.setResponsePayload = function (newPayloadStr) {
     conversationResponsePayloadSetter.call(Conversation, newPayloadStr);
     console.log("Received: " + newPayloadStr);
     var data = JSON.parse(newPayloadStr);
     $("#agent-response").text(data.output.text);
 
-    if(data.context.policy && data.context.verified === true) {
+    if (data.context.policy && data.context.verified === true) {
       Policy.getPolicy(data.context.policy, userLang);
       $("#policy.collapse").collapse('show');
     }
